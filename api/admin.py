01e505f0dc67ce4400a8cc395ac41297ec081172
velvet-noir <this.is.romniko@gmail.com>
2025-05-26 16:33:19 +0700

    создал модели для таблиц и подключил их к админке

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,59 @@
 from django.contrib import admin
+from .models import (
+    Application,
+    ApplicationService,
+    ApplicationStatus,
+    Service,
+    ServiceSpecification,
+)
 
-# Register your models here.
+
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ("name", "image", "price", "is_active")
+    ordering = ("id",)
+
+
+class ServiceSpecificationAdmin(admin.ModelAdmin):
+    list_display = ("get_service_name", "processor", "ram", "disk", "internet_speed")
+    ordering = ("id",)
+
+    def get_service_name(self, obj):
+        return f"Характеристика: {obj.service.name}"
+
+    get_service_name.short_description = "Service"
+
+
+class ApplicationStatusAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "description")
+    ordering = ("id",)
+
+
+class ApplicationAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "get_status_name",
+        "created_at",
+        "update_at",
+        "user_creator",
+        "user_moderator",
+    )
+    ordering = ("created_at",)
+
+    def get_status_name(self, obj):
+        return f"{obj.status.name}"
+
+    get_status_name.short_description = "Status"
+
+
+class ApplicationServiceAdmin(admin.ModelAdmin):
+    list_display = ("id", "application", "get_service_name")
+
+    def get_service_name(self, obj):
+        return f"Характеристика: {obj.service.name}"
+
+
+admin.site.register(Service, ServiceAdmin)
+admin.site.register(ServiceSpecification, ServiceSpecificationAdmin)
+admin.site.register(ApplicationStatus, ApplicationStatusAdmin)
+admin.site.register(Application, ApplicationAdmin)
+admin.site.register(ApplicationService, ApplicationServiceAdmin)
--- api/models.py
+++ api/models.py
@@ -1,3 +1,96 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Service(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.CharField(max_length=200)
+    mini_description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["id"]
+        verbose_name = "Услуга"
+        verbose_name_plural = "Услуги"
+
+
+class ServiceSpecification(models.Model):
+    service = models.ForeignKey(
+        Service, on_delete=models.CASCADE, related_name="specifications"
+    )
+    description = models.TextField()
+    processor = models.CharField(max_length=100)
+    ram = models.CharField(max_length=100)
+    disk = models.CharField(max_length=100)
+    internet_speed = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.service.name
+
+    class Meta:
+        ordering = ["id"]
+        verbose_name = "Характеристика услуги"
+        verbose_name_plural = "Характетистики услуг"
+
+
+class ApplicationStatus(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Статус заявки"
+        verbose_name_plural = "Статусы заявок"
+
+
+class Application(models.Model):
+    status = models.ForeignKey(
+        ApplicationStatus, on_delete=models.CASCADE, related_name="applications"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    user_creator = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="created_applications",
+    )
+    user_moderator = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="moderated_applications",
+    )
+
+    def __str__(self):
+        return f"Заявка № {self.id}"
+
+    class Meta:
+        verbose_name = "Заявка"
+        verbose_name_plural = "Заявки"
+        ordering = ["created_at"]
+
+
+class ApplicationService(models.Model):
+    application = models.ForeignKey(
+        Application, on_delete=models.CASCADE, related_name="services"
+    )
+    service = models.ForeignKey(
+        Service, on_delete=models.CASCADE, related_name="applications"
+    )
+
+    class Meta:
+        unique_together = ("application", "service")
+        verbose_name = "Услуга в заявке"
+        verbose_name_plural = "Услуги в заявках"
+
+    def __str__(self):
+        return f"Заявка {self.application.id} - Услуга {self.service.name}"
