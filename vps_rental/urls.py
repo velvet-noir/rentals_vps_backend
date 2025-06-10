69e714204237ce60b2410d8c4e15e486e97d5f09
velvet-noir <this.is.romniko@gmail.com>
2025-06-10 23:54:45 +0700

    lab_3

--- vps_rental/urls.py
+++ vps_rental/urls.py
@@ -5,4 +5,18 @@ urlpatterns = [
     path(r"services/", views.ServiceList.as_view(), name="services-list"),
     path(r"services-add/", views.ServiceAdd.as_view(), name="services-add"),
     path(r"services/<int:pk>/", views.ServiceDetail.as_view(), name="services-detail"),
+    path(r"app/", views.ApplicationList.as_view(), name="application-list"),
+    path(
+        r"app/<int:pk>/", views.ApplicationDetail.as_view(), name="application-detail"
+    ),
+    path(
+        "app/<int:pk>/formed/",
+        views.ApplicationFormed.as_view(),
+        name="application-formed",
+    ),
+    path(
+        r"app/del/<int:service_id>",
+        views.ApplicationDeleteServer.as_view(),
+        name="remove-service-from-applic",
+    ),
 ]
--- vps_rental/views.py
+++ vps_rental/views.py
@@ -5,8 +5,9 @@ from rest_framework.views import APIView
 from django.db.models import Q
 from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
-from .models import Service
+from .models import Application, ApplicationService, ApplicationStatus, Service
 from .serializers import (
+    ApplicationSerializer,
     ServiceDetailSerializer,
     ServiceSerializer,
 )
@@ -148,5 +149,222 @@ class ServiceDetail(APIView):
         except Exception:
             return Response(
                 {"detail": "Internal server error"},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            )
+
+
+class ApplicationList(APIView):
+    model_class = Application
+    serializer_class = ApplicationSerializer
+
+    @swagger_auto_schema(
+        operation_summary="Получить список всех заявок",
+        manual_parameters=[
+            openapi.Parameter(
+                "status",
+                openapi.IN_QUERY,
+                description="Фильтр по статусу",
+                type=openapi.TYPE_STRING,
+            ),
+        ],
+        responses={200: ApplicationSerializer(many=True)},
+        tags=["applications"],
+    )
+    def get(self, request, format=None):
+        try:
+            applications = self.model_class.objects.exclude(
+                status__in=[ApplicationStatus.DRAFT, ApplicationStatus.DELETED]
+            )
+
+            status_name = request.query_params.get("status")
+            if status_name:
+                applications = applications.filter(status=status_name)
+
+            serializer = self.serializer_class(applications, many=True)
+            return Response(
+                {"status": "success", "data": serializer.data},
+                status=status.HTTP_200_OK,
+            )
+        except Exception as e:
+            return Response(
+                {"status": "error", "detail": str(e)},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            )
+
+
+class ApplicationDetail(APIView):
+    model_class = Application
+    serializer_class = ApplicationSerializer
+
+    @swagger_auto_schema(
+        operation_summary="Получить одну заявку по ID",
+        responses={200: ApplicationSerializer},
+        tags=["application"],
+    )
+    def get(self, request, pk, format=None):
+        try:
+            application = get_object_or_404(self.model_class, pk=pk)
+            serializer = self.serializer_class(application)
+            return Response(
+                {"status": "success", "data": serializer.data},
+                status=status.HTTP_200_OK,
+            )
+        except Exception as e:
+            return Response(
+                {"status": "error", "detail": str(e)},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            )
+
+    @swagger_auto_schema(
+        operation_summary="Модератор меняет статус заявки на completed/rejected",
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            required=["status"],
+            properties={"status": openapi.Schema(type=openapi.TYPE_STRING)},
+        ),
+        responses={200: ApplicationSerializer},
+        tags=["application"],
+    )
+    def put(self, request, pk, format=None):
+        try:
+            application = get_object_or_404(self.model_class, pk=pk)
+
+            new_status = request.data.get("status")
+
+            if new_status not in [
+                ApplicationStatus.COMPLETED,
+                ApplicationStatus.REJECTED,
+            ]:
+                return Response(
+                    {"detail": "Invalid status. Allowed: COMPLETED or REJECTED."},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+
+            application.status = new_status
+            application.save()
+
+            serializer = self.serializer_class(application)
+            return Response(
+                {
+                    "status": "success",
+                    "data": serializer.data,
+                    "detail": f"The application status has been successfully changed to '{new_status}'",
+                },
+                status=status.HTTP_200_OK,
+            )
+        except Exception as e:
+            return Response(
+                {"status": "error", "detail": str(e)},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            )
+
+    @swagger_auto_schema(
+        operation_summary="Удалить заявку через смену статуса на 'deleted'",
+        responses={200: ApplicationSerializer},
+        tags=["application"],
+    )
+    def delete(self, request, pk, format=None):
+        try:
+            application = get_object_or_404(self.model_class, pk=pk)
+
+            application.status = ApplicationStatus.DELETED
+            application.save()
+
+            serializer = self.serializer_class(application)
+            return Response(
+                {
+                    "status": "success",
+                    "data": serializer.data,
+                    "detail": "The application has been successfully marked as deleted",
+                },
+                status=status.HTTP_200_OK,
+            )
+        except Exception as e:
+            return Response(
+                {"status": "error", "detail": str(e)},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            )
+
+
+class ApplicationFormed(APIView):
+    model_class = Application
+    serializer_class = ApplicationSerializer
+
+    @swagger_auto_schema(
+        operation_summary="Установить для заявки статус 'formed(сформирована)'",
+        responses={200: ApplicationSerializer},
+        tags=["application/formed"],
+    )
+    def put(self, request, pk, format=None):
+        try:
+            application = get_object_or_404(self.model_class, pk=pk)
+
+            application.status = ApplicationStatus.FORMED
+            application.save()
+
+            serializer = self.serializer_class(application)
+            return Response(
+                {
+                    "status": "success",
+                    "data": serializer.data,
+                    "detail": "The application status has been successfully changed to 'Formed'",
+                },
+                status=status.HTTP_200_OK,
+            )
+        except Exception as e:
+            return Response(
+                {"status": "error", "detail": str(e)},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            )
+
+
+class ApplicationDeleteServer(APIView):
+    @swagger_auto_schema(
+        operation_summary="Удаление сервера по ID из черновой заявки пользователя",
+        responses={204: "No Content"},
+        tags=["application/delete-service"],
+    )
+    def delete(self, request, service_id, format=None):
+        try:
+            # application = Application.objects.filter(
+            #     user_creator=request.user, status=ApplicationStatus.DRAFT
+            # ).first()
+            application = Application.objects.filter(status=ApplicationStatus.DRAFT).first()
+
+            if not application:
+                return Response(
+                    {
+                        "status": "error",
+                        "detail": "Черновая заявка не найдена",
+                    },
+                    status=status.HTTP_404_NOT_FOUND,
+                )
+
+            app_service = get_object_or_404(
+                ApplicationService,
+                application=application,
+                service_id=service_id,
+            )
+
+            if application.status != ApplicationStatus.DRAFT:
+                return Response(
+                    {
+                        "status": "error",
+                        "detail": "Удаление сервера возможно только для заявок со статусом DRAFT.",
+                    },
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+
+            app_service.delete()
+            return Response(
+                {
+                    "status": "success",
+                    "detail": "Сервер успешно удалён из черновой заявки.",
+                },
+                status=status.HTTP_200_OK,
+            )
+        except Exception as e:
+            return Response(
+                {"status": "error", "detail": str(e)},
                 status=status.HTTP_500_INTERNAL_SERVER_ERROR,
             )
